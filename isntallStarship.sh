d966f845c1ece4162bab7bfe1099aae1d7f483bf
3lLobo <flocwolf@gmail.com>
2023-03-13 23:01:18 +0100

    update 2023-03-13

---
+++ isntallStarship.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install starship, add it to all rc files, and copy the toml.
+curl -fsSL https://starship.rs/install.sh | sh -s -- -y
+echo '\neval "$(starship init bash)"' >>~/.bashrc
+echo '\neval "$(starship init bash)"' >>~/.zshrc
+
+# Copy the starship.toml file to the home directory.
+cp starship.toml ~/.config/starship.toml
+echo "Starship installed 💫"
